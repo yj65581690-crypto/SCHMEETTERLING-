50bcf9ed10035930e4228438a8795ce262143551
sdsdss <yj65581690@gmail.com>
2026-08-21 10:43:53 +0900

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const loginScreen = document.getElementById("loginScreen");
 
 
 
-
 // SCHMEETTERLING 직원 DB
 
 const users = {
@@ -35,152 +34,70 @@ const users = {
 
 
 
-
-// 시작 화면 → 로그인 화면
+// 로그인 화면 열기
 
 if(loginButton){
 
-    loginButton.onclick = function(){
+loginButton.onclick=function(){
 
-        startScreen.style.display = "none";
+    startScreen.style.display="none";
 
-        loginScreen.style.display = "block";
+    loginScreen.style.display="block";
 
-    };
+};
 
 }
 
 
 
 
-
-
-
-// 로그인 처리
+// 로그인
 
 if(enterButton){
 
-    enterButton.onclick = function(){
+enterButton.onclick=function(){
 
 
-        let name =
-        document.getElementById("username").value.trim();
+let name =
+document.getElementById("username").value.trim();
 
 
-        let password =
-        document.getElementById("password").value.trim();
-
-
-
-
-
-        if(users[name] && users[name].password === password){
+let password =
+document.getElementById("password").value.trim();
 
 
 
-            localStorage.setItem(
-                "userName",
-                name
-            );
+if(users[name] && users[name].password === password){
 
 
-            localStorage.setItem(
-                "userLevel",
-                users[name].level
-            );
+    localStorage.setItem(
+        "userName",
+        name
+    );
 
 
+    localStorage.setItem(
+        "userLevel",
+        users[name].level
+    );
 
 
+    // 로딩 화면 이동
 
-            location.href="./loading.html";
+    location.href="./loading.html";
 
-
-
-        }
-
-
-
-        else{
-
-
-            alert("접근 권한이 없습니다.");
-
-
-        }
-
-
-
-    };
 
 }
 
 
+else{
+
+    alert("접근 권한이 없습니다.");
+
+}
 
 
+};
 
 
-// 접속자 표시
-
-window.addEventListener("load", function(){
-
-
-
-    let name =
-    localStorage.getItem("userName");
-
-
-    let level =
-    localStorage.getItem("userLevel");
-
-
-
-
-    if(document.querySelector(".userInfo")){
-
-        return;
-
-    }
-
-
-
-
-    if(name){
-
-
-
-        let box =
-        document.createElement("div");
-
-
-
-        box.className="userInfo";
-
-
-
-        box.innerHTML = `
-
-        현재 접속자 :
-        <span>${name}</span>
-
-        <br>
-
-        LEVEL :
-        <span>${level}</span>
-
-        <br><br>
-
-
-        `;
-
-
-
-        document.body.appendChild(box);
-
-
-
-    }
-
-
-
-});
+}
